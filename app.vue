14a2f8779e89ac8ecf372ab1e914a28f8b0abb6e
Matea <matheusnsantana@gmail.com>
2024-03-08 16:23:24 -0300

    Footer & Header
    [Add] Layout
    [Add] Header
    [Add] Footer
    [To Fix] Responsividade do Header
    [To Do] Adicionar Icones no Footer
    [Extra] Add dependencias de fonts no qual tem os icones, mas ainda nao funcionam

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <div>
+      <p>Hello World</p>
+    </div>
+    <v-expansion-panels>
+      <v-expansion-panel
+        title="Title"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima"
+      >
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </NuxtLayout>
 </template>
+
+<style scoped>
+  .page {
+    background-color: white;
+  }
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-container class="header" fluid>
+        <v-row class="d-flex align-center">
+            <v-col offset="1" cols="1">
+                <span class="">BACK</span>
+            </v-col>
+            <v-col lg="3" md="2" sm="1"></v-col>
+            <v-col class="d-flex justify-center" cols="2">
+                <span class="text-h4 font-weight-bold"><span style="color: white;">VET</span>SEARCH</span>
+            </v-col>
+            <v-col class="d-flex justify-end pr-0" cols="2" offset="">
+                <span class="">MY APPOINTMENTS</span>
+            </v-col>
+            <v-col class="d-flex justify-end" cols=2>
+                <!-- <span class="pl-15">MY APPOINTMENTS</span> -->
+                <span class="">MY ACCOUNT</span>
+            </v-col>
+            <v-col cols=1></v-col>
+        </v-row>
+    </v-container>
+    <div>
+        <slot />
+    </div>
+    <v-container fluid class="footer">
+        <v-row>
+            <v-col cols="1"></v-col>
+            <v-col cols="4">
+                <span class="text-sm-h6 text-body-1 font-weight-bold"><span style="color: white;">VET</span>SEARCH</span>
+            </v-col>
+            <v-col cols="6">
+                <div class="d-flex flex-column align-end">
+                    <span class="text-body-2 text-sm-body-1 font-weight-bold">MY ACCOUNT</span>
+                    <span class="text-body-2 text-sm-body-1 font-weight-bold">MY APPOINTMENTS</span>
+                </div>
+            </v-col>
+            <v-col cols="1"></v-col>
+        </v-row>
+        <v-row>
+            <v-icon icon="fa:fas fa-lock"></v-icon>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .header {
+        background-color: #1a1939;
+        color: #caddff;
+        font-weight: 600;
+    }
+    .footer {
+        background-color: #1a1939;
+        color: #caddff;
+    }
+</style>
